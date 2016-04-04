30c20d197b1059f2040935a0f4cde5abb2feb1cb
George Vorobyev <quaffle97@gmail.com>
2016-04-03 20:26:27 -0400

    Cleaned up player.js

--- player.js
+++ player.js
@@ -25,7 +25,11 @@ var Constants = Constants || {};
 	};
 }
 
-function player(){
+var player;
+{ //Player declaration scope
+var s = Constants.player.jumpstates;
+
+player = function(){
 	this.bounds = new bounds(new Vector(2,1), Constants.player.size.clone());
 
 	this.bounds.onCollide = function(speed){
@@ -42,80 +46,82 @@ function player(){
 	this.jumpspeed    = Constants.player.jumpspeed;
 	this.jetpackspeed = Constants.player.jetpackspeed;
 
-	var s = Constants.player.jumpstates;
 	this.jumpState = s.NOT_PRESSED;
+}
 
-	this.update = function(time){
-		var jumpKey = keyState[87] || keyState[38] || keyState[90];
+player.prototype.update = function(time){
+	//Get gravitied
+	this.bounds.vel.add(0, this.gravity * time);
+	this.bounds.vel.x = 0;	
+	
+	//Move left/right
+	if(keyState[37] || keyState[65])
+		this.bounds.vel.x = -1 * this.movespeed;
+	if(keyState[39] || keyState[68])
+		this.bounds.vel.x += this.movespeed;
 
-		this.bounds.vel.add(0, this.gravity * time);
-		this.bounds.vel.x = 0;	
-		
-		//Move left
-		if(keyState[37] || keyState[65])
-			this.bounds.vel.x = -1 * this.movespeed;
-			
-		//Move right
-		if(keyState[39] || keyState[68])
-			this.bounds.vel.x += this.movespeed;
-
-		this.bounds.move(time);
-		
-		//Update facing (for jetpack mount point)
-		if(this.bounds.vel.x != 0) {
-			this.facing = this.bounds.vel.x>0?1:-1;
-			this.offset.setV(Constants.player.jetpackOffset[this.getFacingIndex()]);
-		}
-
-		//Handle jump (On ground)
-		if(this.bounds.onGround){
-			//reset keys
-			if(this.jumpState === s.BUTTON_IN_AIR)
-				this.jumpState = s.NOT_PRESSED;
-			if(!jumpKey)
-				{ this.jumpState = s.NOT_PRESSED; }
-
-			//Do normal jump
-			else if(jumpKey && this.jumpState === s.NOT_PRESSED) {
-				this.jumpState = s.BUTTON;
-				this.bounds.vel.y = -1 * this.jumpspeed;
-			}
-
-		} 
-
-		//Handle jump (In air)
-		else{
-			if(!jumpKey) //Reset button
-				{ this.jumpState = s.NOT_PRESSED; }
-
-			//do jetpack, unless is normal jump and not yet been released
-			else if(jumpKey && this.jumpState != s.BUTTON){
-				if(this.jumpState === s.NOT_PRESSED) {//if just started
-					this.pack.burst(Constants.player.jetpackburst); 
-					this.bounds.vel.y = Constants.player.jetpackburstspeed + this.jetpackspeed;
-				}
-				this.jumpState = s.BUTTON_AIR;
-				this.bounds.vel.y = Math.min(this.bounds.vel.y, -1 * this.jetpackspeed);
-				this.pack.update(time);
-			}
-		}	
-
-		//Water cannon
-		if(keyState[88]){
-			var temp = this.bounds.pos.clone();
-			temp.addV(Constants.player.waterCannonOffset[this.getFacingIndex()]);
-			this.waterCannon.update(time,temp,this.facing);
-		}
-
-
+	//Update facing (for jetpack mount point)
+	if(this.bounds.vel.x != 0) {
+		this.facing = this.bounds.vel.x>0?1:-1;
+		this.offset.setV(Constants.player.jetpackOffset[this.getFacingIndex()]);
 	}
 
-	//returns 0 if facing right, 1 otherwise
-	this.getFacingIndex = function() 
-		{return this.facing>0?0:1;}
+	//Move by velocity
+	this.bounds.move(time);
 
-	this.draw = function(ctx){
-		ctx.fillStyle="#88F";
-		ctx.fillRect(cellSize * this.bounds.pos.x, cellSize * this.bounds.pos.y, cellSize * this.bounds.size.x, cellSize * this.bounds.size.y);
+	//Handle Jump input
+	var jumpKeyState = keyState[87] || keyState[38] || keyState[90];
+	this.handleJumping(jumpKeyState, time);
+
+	//Handle water cannon input
+	if(keyState[88]){ //'x'
+		var temp = this.bounds.pos.clone();
+		temp.addV(Constants.player.waterCannonOffset[this.getFacingIndex()]);
+		this.waterCannon.update(time,temp,this.facing);
 	}
 }
+
+player.prototype.handleJumping = function(jumpKeyState, time) {
+	//Handle jump (On ground)
+	if(this.bounds.onGround){
+		//reset keys
+		if(this.jumpState === s.BUTTON_IN_AIR)
+			this.jumpState = s.NOT_PRESSED;
+		if(!jumpKeyState)
+			{ this.jumpState = s.NOT_PRESSED; }
+
+		//Do normal jump
+		else if(jumpKeyState && this.jumpState === s.NOT_PRESSED) {
+			this.jumpState = s.BUTTON;
+			this.bounds.vel.y = -1 * this.jumpspeed;
+		}
+	} 
+
+	//Handle jump (In air)
+	else{
+		if(!jumpKeyState) //Reset button
+			{ this.jumpState = s.NOT_PRESSED; }
+
+		//do jetpack, unless is normal jump and not yet been released
+		else if(jumpKeyState && this.jumpState != s.BUTTON){
+			if(this.jumpState === s.NOT_PRESSED) {//if just started
+				this.pack.burst(Constants.player.jetpackburst); 
+				this.bounds.vel.y = Constants.player.jetpackburstspeed + this.jetpackspeed;
+			}
+			this.jumpState = s.BUTTON_AIR;
+			this.bounds.vel.y = Math.min(this.bounds.vel.y, -1 * this.jetpackspeed);
+			this.pack.update(time);
+		}
+	}	
+}
+
+//returns 0 if facing right, 1 otherwise
+player.prototype.getFacingIndex = function() 
+	{return this.facing>0?0:1;}
+
+player.prototype.draw = function(ctx){
+	ctx.fillStyle="#88F";
+	ctx.fillRect(cellSize * this.bounds.pos.x, cellSize * this.bounds.pos.y, cellSize * this.bounds.size.x, cellSize * this.bounds.size.y);
+}
+
+}//End player declaration scope
